8b74cd5dcd3744935ea21826300558feb71dc834
userName <userEmail>
2025-11-01 22:38:02 +0800

    作业看板

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/proj_fz/HomeworkStatisticsController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/proj_fz/HomeworkStatisticsController.java
@@ -1,4 +1,4 @@
-package com.ruoyi.proj_fz.controller;
+package com.ruoyi.web.controller.proj_fz;
 
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
